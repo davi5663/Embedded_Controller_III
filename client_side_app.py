1b4e19922ebde71c053f642c52c416dce556707e
davi5663 <46707547+davi5663@users.noreply.github.com>
2022-11-07 09:19:26 +0100

    Update client_side_app.py

--- client_side_app.py
+++ client_side_app.py
@@ -5,7 +5,7 @@ from time import sleep
 sensor = 2
 blue = 0    
 white = 1   
-url = "http://192.168.73.179:5000/api"
+url = "http://1xx.xxx.xx.xxx:5000/api"
 while True:
         [tempature,humidity] = grovepi.dht(sensor,blue) 
         sleep(120)
